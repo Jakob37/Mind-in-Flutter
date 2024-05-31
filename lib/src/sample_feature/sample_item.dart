7703d192094c86b309ef2220c739132d7aa4c0f5
Jakob Willforss <jakob.willforss@hotmail.com>
2024-05-31 12:11:52 +0200

    Adding items runs

--- lib/src/sample_feature/sample_item.dart
+++ lib/src/sample_feature/sample_item.dart
@@ -1,8 +1,7 @@
 /// A placeholder class that represents an entity or model.
 class SampleItem {
-  const SampleItem(this.id, this.id2, this.content);
+  const SampleItem(this.id, this.content);
 
   final int id;
-  final int id2;
   final String content;
 }
--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -5,22 +5,26 @@ import 'sample_item.dart';
 import 'sample_item_details_view.dart';
 
 /// Displays a list of SampleItems.
-class SampleItemListView extends StatelessWidget {
-  const SampleItemListView({
-    super.key,
-    this.items = const [
-      SampleItem(1, 10, "I want to capture learning"),
-      SampleItem(2, 20, "I want to capture idea building"),
-      SampleItem(3, 30, "I want to iterate organically on this app"),
-      SampleItem(3, 30, "Learning is also a priority"),
-      SampleItem(3, 30,
-          "Looks like next target is to get user editing and data persistance"),
-    ],
-  });
+class SampleItemListView extends StatefulWidget {
+  const SampleItemListView({Key? key}) : super(key: key);
 
   static const routeName = '/';
 
-  final List<SampleItem> items;
+  @override
+  _SampleItemListViewState createState() => _SampleItemListViewState();
+}
+
+class _SampleItemListViewState extends State<SampleItemListView> {
+  List<SampleItem> items = [
+    SampleItem(1, "I want to capture learning"),
+    SampleItem(2, "I want to capture idea building"),
+    SampleItem(3, "I want to iterate organically on this app"),
+    SampleItem(4, "Learning is also a priority"),
+    SampleItem(5,
+        "Looks like next target is to get user editing and data persistance"),
+  ];
+
+  // final List<SampleItem> items;
 
   @override
   Widget build(BuildContext context) {
@@ -31,25 +35,12 @@ class SampleItemListView extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
               Navigator.restorablePushNamed(context, SettingsView.routeName);
             },
           ),
         ],
       ),
-
-      // To work with lists that may contain a large number of items, it’s best
-      // to use the ListView.builder constructor.
-      //
-      // In contrast to the default ListView constructor, which requires
-      // building all Widgets up front, the ListView.builder constructor lazily
-      // builds Widgets as they’re scrolled into view.
       body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
         restorationId: 'sampleItemListView',
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
@@ -87,7 +78,15 @@ class SampleItemListView extends StatelessWidget {
       context: context,
       builder: (BuildContext context) => _InputModal(
         onSubmitted: (value) {
-          print("Received input: $value");
+          setState(() {
+            int nextId = items.fold(
+                    0,
+                    (int currentMax, SampleItem item) =>
+                        item.id > currentMax ? item.id : currentMax) +
+                1;
+            items.add(SampleItem(nextId, value));
+          });
+          // Navigator.of(context).pop();
         },
       ),
     );
